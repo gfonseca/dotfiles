2ec32aa641d5bb5fd4adfa0acbbae9f2daea0e3d
Georgio Barbosa da Fonseca <georgio.fonseca@mercadolivre.com>
2024-06-09 10:59:07 -0300

    adding doc to alias

--- alias.sh
+++ alias.sh
@@ -2,73 +2,75 @@
 
 unalias -a
 
-#GENERAL
-alias ls="lsd"
-alias ll="lsd -halt"
-alias kgen="ssh-keygen -t ed25519"
-
-#ZSH
-alias zshconfig="code $HOME/.zshrc"
-alias szsh="source $HOME/.zshrc"
-
-## K8S
+#>  -- GENERAL --
+alias ls="lsd" #> lsd: list folder content
+alias ll="lsd -halt" #> ll: list folder human readable
+alias kgen="ssh-keygen -t ed25519" #> kgen: generate ssh key pair 
+#>  --------
+#> -- ZSH --
+alias zshconfig="code $HOME/.zshrc" #> zshconfig: open zsh config file
+alias szsh="source $HOME/.zshrc" #> szsh: source zsh config file
+#>  --------
+#> -- K8S --
 if command -v kubectl > /dev/null 2>&1; then
-    kns () {
+    kns () { #> kns: kubectl namespace switch
         export NAMES_SPACE=$1
     }
     
     source <(kubectl completion zsh)
-    alias k='kubectl -n $NAMES_SPACE'
-    alias kg='kubectl get -n $NAMES_SPACE'
-    alias ka='kubectl apply -n $NAMES_SPACE'
-    alias kl='kubectl logs -n $NAMES_SPACE'
+    alias k='kubectl -n $NAMES_SPACE' #> k: kubectl
+    alias kg='kubectl get -n $NAMES_SPACE' #> kg: kubectl get
+    alias ka='kubectl apply -n $NAMES_SPACE' #> ka: kubectl apply
+    alias kl='kubectl logs -n $NAMES_SPACE' #> kl: kubectl logs
 fi
-
-#GIT
+#>  --------
+#> -- GIT --
 if command -v git > /dev/null 2>&1; then
-    alias gs='git status'
-    alias gl='git log'
-    alias gs='git push'
-    alias gc='git commit'
+    alias gs='git status' #> gs: git status
+    alias gl='git log' #> gl: git log
+    alias gs='git push' #> gs: git push
+    alias gc='git commit' #> gc: git commit
 fi
 
-#DOCKER
+#>  -----------
+#> -- DOCKER --
 if command -v docker > /dev/null 2>&1; then
-    alias dps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"'
-    alias dcu='docker-compose up -d'
-    alias dcs='docker-compose stop'
-    alias dcb='docker-compose build'
-    alias dcub='docker-compose up -d --build'
-    alias dcr='docker-compose restart'
-    alias dcd='docker-compose down'
-    alias dcl='docker-compose logs -f'
+    alias dps='docker ps --format "table {{.ID}}\t{{.Names}}\t{{.Status}}\t{{.Ports}}"' #> dps: docker ps
+    alias dcu='docker-compose up -d' #> dcu: docker-compose up
+    alias dcs='docker-compose stop' #> dcs: docker-compose stop
+    alias dcb='docker-compose build' #> dcb: docker-compose build
+    alias dcub='docker-compose up -d --build' #> dcub: docker-compose up --build
+    alias dcr='docker-compose restart' #> dcr: docker-compose restart
+    alias dcd='docker-compose down' #> dcd: docker-compose down
+    alias dcl='docker-compose logs -f' #> dcl: docker-compose logs
 fi
 
-#PACKAGE MANAGERS
+#>  ----------------------
+#>  -- PACKAGE MANAGERS --
 if command -v brew > /dev/null 2>&1; then
-    brewf() {
+    brewf() { #> brewf: brew search
         brew search $1
     }
 
-    brewi () {
+    brewi () { #> brewi: brew info
         brew info $1
     }
 
-    alias brewc='brew cleanup'
-    alias update='brew update && brew upgrade && brew cleanup'
+    alias brewc='brew cleanup' #> brewc: brew cleanup
+    alias update='brew update && brew upgrade && brew cleanup' #> update: brew update && brew upgrade && brew cleanup
 fi
 
 if command -v apt-get > /dev/null 2>&1; then
-    aptf() {
+    aptf() { #> aptf: apt-cache search
         apt-cache search $1
     }
 
-    apti () {
+    apti () { #> apti: apt-cache show
         apt-cache show $1
     }
 
-    alias aptc='sudo apt-get autoremove && sudo apt-get autoclean'
-    alias update='sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove && sudo apt-get autoclean'
+    alias aptc='sudo apt-get autoremove && sudo apt-get autoclean' #> aptc: apt-get autoremove && apt-get autoclean
+    alias update='sudo apt-get update && sudo apt-get upgrade && sudo apt-get autoremove && sudo apt-get autoclean' #> update: apt-get update && apt-get upgrade && apt-get autoremove && apt-get autoclean
 fi
 
-alias shortk='grep -E "^\s*alias|^.*\(\)" $HOME/.config/myenv/alias.sh'
+alias shortk='grep -E "#>" $MYENV_PATH/alias.sh | awk -F"#>" "{ sub(/^> /, \"\", \$2); print \$2 }" | sed "s/^[ \t]*//;s/[ \t]*$//"' #> shortk: show aliases
