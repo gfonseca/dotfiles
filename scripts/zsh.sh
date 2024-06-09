4907a34ef6a482f68c270fcb8ba4fefe98725993
Georgio Barbosa da Fonseca <georgio.fonseca@mercadolivre.com>
2024-06-09 18:03:00 -0300

    adding source powerline

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -3,10 +3,10 @@
 if [ ! -d ~/.oh-my-zsh/custom/themes/powerlevel9k ]; then
     git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 fi
-
+source  ~/.oh-my-zsh/custom/themes/powerlevel9k/powerlevel9k.zsh-theme
 ZSH_THEME="powerlevel9k/powerlevel9k"
+POWERLEVEL9K_MODE='nerdfont-complete'
 POWERLEVEL9K_DISABLE_RPROMPT=true
-POWERLEVEL9K_PROMPT_ON_NEWLINE=true
-POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="▶ "
-POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=""
-POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status virtualenv)
+POWERLEVEL9K_PROMPT_ON_NEWLINE=trues
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context dir newline)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(vcs)
